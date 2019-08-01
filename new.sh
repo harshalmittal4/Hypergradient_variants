4eb906b9da7ccad9938354baeb7ea0641879d6f6
Harshal Mittal <harshalmittal4@gmail.com>
2019-08-01 09:39:59 +0530

    Add prev exp left to new.sh

--- new.sh
+++ new.sh
@@ -1,8 +1,18 @@
+# new exp
 python train.py --cuda --method opSgd_lopAdam --model logreg --epochs 20 --dir lopAdam --save --beta 0.001
 python train.py --cuda --method opAdam_lopAdam --model logreg --epochs 20 --dir lopAdam --save --beta 0.0000001
 
 python train.py --cuda --method opSgd_lopAdam --model vgg --epochs 200 --dir lopAdam --save --beta 0.001
 python train.py --cuda --method opAdam_lopAdam --model vgg --epochs 200 --dir lopAdam --save --beta 1e-8
+
+#prev wale
+python train.py --cuda --method opSgdn_lopSgdn --mu 0.9 --model vgg --epochs 200 --dir test --save --beta 0.001
+python train.py --cuda --method opSgdn_lopAdam --mu 0.9 --model vgg --epochs 200 --dir test --save --beta 0.001
+python train.py --cuda --method sgd --model logreg --epochs 20 --dir test --save
+
+
+
+
 
 
 
