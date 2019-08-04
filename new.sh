39fa5a20a70a35ccdc6d999d13d29dfd7946ec84
Ankit Dhankhar <adhankhar@cs.iitr.ac.in>
2019-08-05 04:31:15 +0530

    updated reaults

--- new.sh
+++ new.sh
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
-# new exp
 # python train.py --cuda --method opSgd_lopAdam --model logreg --epochs 20 --dir lopAdam --save --beta 0.001
 # python train.py --cuda --method opAdam_lopAdam --model logreg --epochs 20 --dir lopAdam --save --beta 0.0000001
-=======
-python train.py --cuda --method opSgd_lopAdam --model logreg --epochs 20 --dir lopAdam --save --beta 0.001
-python train.py --cuda --method opAdam_lopAdam --model logreg --epochs 20 --dir lopAdam --save --beta 0.0000001
->>>>>>> parent of 4eb906b... Add prev exp left to new.sh
 
-# python train.py --cuda --method opSgd_lopAdam --model vgg --epochs 200 --dir lopAdam --save --beta 0.001
-python train.py --cuda --method opAdam_lopAdam --model vgg --epochs 200 --dir lopAdam --save --beta 1e-8
+python train.py --cuda --method opSgd_lopAdam --model vgg --epochs 200 --dir lopAdam --save --beta 0.001
+# python train.py --cuda --method opAdam_lopAdam --model vgg --epochs 200 --dir lopAdam --save --beta 1e-8
 
 
 
