75037f1c2fbcefe3e483610143e4c3f22b8814e9
Harshal Mittal <harshalmittal4@gmail.com>
2019-08-12 05:46:31 +0530

    Fix new.sh

--- new.sh
+++ new.sh
@@ -23,24 +23,26 @@ python train.py --cuda --method adam_Hdmomentum --model mlp --epochs 100 --dir t
 python train.py --cuda --method adam_HdAdam --model mlp --epochs 100 --dir test --save --beta 0.0000001
 
 
-for optim in ['sgd_HdAdam', 'sgd_Hd', 'sgd_Hdmomentum', 'sgd', 'sgdn_Hd','sgdn_Hdmomentum', 'sgdn']:
+for optim in 'sgd_HdAdam' 'sgd_Hd' 'sgd_Hdmomentum' 'sgd' 'sgdn_Hd' 'sgdn_Hdmomentum' 'sgdn'
 do
-	if optim in ['sgd_HdAdam', 'sgd_Hd', 'sgd_Hdmomentum', 'sgd']:
+	
+	if  [ $optim = 'sgd_HdAdam' ] || [ $optim = 'sgd_Hd' ] || [ $optim = 'sgd_Hdmomentum' ] || [ $optim = 'sgd' ]
 	then
-		t = 0.75
+		t=0.75
 	else
-		t = 0.3
+		t=0.3
 	fi
-	for alpha0 in [0.01, 0.005, 0.001, 0.0005, 0.0001]:
+	
+	for alpha0 in 0.01 0.005 0.001 0.0005 0.0001 
     do
     	python train.py --cuda --method $optim --model vgg --dir Alpha_exp --save --beta 0.001 --alpha_0 $alpha0 --lossThreshold $t
     done
 done
 
 
-for optim in ['adam_HdAdam', 'adam', 'adam_Hd']:
+for optim in 'adam_HdAdam' 'adam' 'adam_Hd'
 do
-	for alpha0 in [0.01, 0.005, 0.001, 0.0005, 0.0001]:
+	for alpha0 in 0.01 0.005 0.001 0.0005 0.0001
     do
     	python train.py --cuda --method $optim --model vgg --dir Alpha_exp --save --beta 1e-8 --alpha $alpha0 --lossThreshold 0.2
     done
