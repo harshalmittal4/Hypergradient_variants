68647b9b8269a39a9bc45613ede8a418ca253fc6
Harshal Mittal <harshalmittal4@gmail.com>
2019-08-19 01:11:31 +0530

    Organise files

--- cal_run.sh
+++
@@ -1,5 +0,0 @@
-python train.py --cuda --method sgdn --mu 0.9 --model vgg --epochs 200 --dir test --save
-python train.py --cuda --method opSgdn_lopHd --mu 0.9 --model vgg --epochs 200 --dir test --save --beta 0.001
-python train.py --cuda --method opSgdn_lopSgdn --mu 0.9 --model vgg --epochs 200 --dir test --save --beta 0.001
-python train.py --cuda --method opSgdn_lopAdam --mu 0.9 --model vgg --epochs 200 --dir test --save --beta 0.001
-
--- walle_run.sh
+++
@@ -1,6 +0,0 @@
-python train.py --cuda --method adam --model vgg --epochs 200 --dir test --save
-python train.py --cuda --method opAdam_lopHd --model vgg --epochs 200 --dir test --save --beta 1e-8
-python train.py --cuda --method opAdam_lopSgdn --model vgg --epochs 200 --dir test --save --beta 1e-8
-python train.py --cuda --method opAdam_lopAdam --model vgg --epochs 200 --dir test --save --beta 1e-8
-
-
